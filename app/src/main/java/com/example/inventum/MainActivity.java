73148d4a334b20fa1ac2074447d8e71518fedd02
Austin Yeung <ahyeung@wisc.edu>
2021-11-03 16:50:56 -0500

    commit

--- app/src/main/java/com/example/inventum/MainActivity.java
+++ app/src/main/java/com/example/inventum/MainActivity.java
@@ -2,9 +2,25 @@ package com.example.inventum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.Spotify;
+import com.spotify.sdk.android.player.SpotifyPlayer;
+
+public class MainActivity extends Activity implements
+        SpotifyPlayer.NotificationCallback, ConnectionStateCallback
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
