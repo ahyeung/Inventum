c48ac9db84a03fd9dfc49a9851bc637872198d13
Austin Yeung <ahyeung@wisc.edu>
2021-12-11 09:38:03 -0600

    commit

--- app/src/main/java/com/example/inventum/SearchFragment.java
+++ app/src/main/java/com/example/inventum/SearchFragment.java
@@ -1,14 +1,19 @@
 package com.example.inventum;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment implements View.OnClickListener {
 
     public SearchFragment() {
         // Required empty public constructor
@@ -17,7 +22,19 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_search, container, false);
+        Button b = (Button) v.findViewById(R.id.advancedSearch);
+        b.setOnClickListener(this);
+        return v;
+    }
+
+    public void onClick(View view) {
+//        Fragment fragment = new AdvSearchFragment();
+        FragmentManager manager = getParentFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.search, new AdvSearchFragment()).commit();
+
+
     }
 }
