091d45192957531db3580995981052142b041b59
melmae <43497997+melmae@users.noreply.github.com>
2021-12-12 18:49:13 -0600

    Track/artist advanced search

--- app/src/main/java/com/example/inventum/CustomAdapter.java
+++ app/src/main/java/com/example/inventum/CustomAdapter.java
@@ -34,6 +34,9 @@ public class CustomAdapter extends BaseAdapter implements ListAdapter {
     private Activity activity;
     SharedPreferences sharedPreferences;
 
+    private String id;
+    private int tag;
+
     public CustomAdapter(ArrayList<String> list, Context context, Activity activity) {
         this.list = list;
         this.context = context;
@@ -270,6 +273,8 @@ public class CustomAdapter extends BaseAdapter implements ListAdapter {
                 activity.findViewById(R.id.findAdvGenreResults).setVisibility(View.INVISIBLE);
 
                 Button findAdvResults = (Button) activity.findViewById(R.id.findAdvResults);
+                id = SearchFragment.idList.get((Integer)v.getTag());
+                tag = (Integer)v.getTag();
                 findAdvResults.setOnClickListener(new View.OnClickListener(){
                     @Override
                     public void onClick(View v) {
@@ -293,6 +298,166 @@ public class CustomAdapter extends BaseAdapter implements ListAdapter {
                         int speechinessBarInt = speechinessBar.getProgress();
                         int instrumentalnessBarInt = instrumentalnessBar.getProgress();
 
+                        float danceabilityBarFloat = danceabilityBarInt/100;
+                        float livenessBarFloat = livenessBarInt/100;
+                        float energyBarFloat = energyBarInt/100;
+                        float valenceBarFloat = valenceBarInt/100;
+                        float speechinessBarFloat = speechinessBarInt/100;
+                        float instrumentalnessBarFloat = instrumentalnessBarInt/100;
+                        float acousticnessBarFloat = acousticnessBarInt/100;
+
+                        RequestQueue queue = Volley.newRequestQueue(context.getApplicationContext());
+
+                        Response.Listener<String> search_listener = new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                Log.d("Advanced", response.substring(0,100));
+                                try {
+                                    JSONObject tracksObject = new JSONObject(response);
+                                    Log.d("Home", tracksObject.toString());
+
+                                    String trackIDs = RemoteAPI.parseRecommendationsResult(tracksObject);
+                                    Log.d("Home", trackIDs);
+
+                                    final JSONObject[] tracks = {null};
+                                    final JSONObject[] tracks_expanded = {null};
+
+                                    Response.Listener<String> track_listener = new Response.Listener<String>() {
+                                        @Override
+                                        public void onResponse(String response) {
+                                            // Display the first 500 characters of the response string.
+                                            Log.d("Authenticated", response.substring(0,500));
+                                            try {
+                                                tracks[0] = new JSONObject(response);
+
+                                                Response.Listener<String> expanded_listener = new Response.Listener<String>() {
+                                                    @Override
+                                                    public void onResponse(String response) {
+                                                        // Display the first 500 characters of the response string.
+                                                        Log.d("Authenticated", response.substring(0,100));
+                                                        try {
+                                                            tracks_expanded[0] = new JSONObject(response);
+
+                                                            ArrayList<invTrack> trackArrayList = new ArrayList<>();
+
+
+                                                            JSONArray tracksArray = tracks[0].getJSONArray("tracks");
+                                                            JSONArray expandedArray = tracks_expanded[0].getJSONArray("audio_features");
+                                                            for (int i = 0; i < tracksArray.length(); i ++) {
+                                                                JSONObject trackObject = tracksArray.getJSONObject(i);
+                                                                JSONObject expandedObject = expandedArray.getJSONObject(i);
+
+                                                                // Get artists and put into string array
+                                                                String[] artists = new String[10];
+                                                                JSONArray artistArray = trackObject.getJSONArray("artists");
+                                                                for (int j = 0; j < artistArray.length(); j++) {
+                                                                    artists[j] = artistArray.getJSONObject(j).getString("name");
+                                                                }
+
+                                                                // Parse the two responses to get the desired information
+                                                                trackArrayList.add(new invTrack(
+                                                                        trackObject.getString("id"),
+                                                                        trackObject.getString("name"),
+                                                                        artists,
+                                                                        trackObject.getJSONObject("album").getString("name"),
+                                                                        trackObject.getJSONObject("album").getString("type"),
+                                                                        trackObject.getJSONObject("album").getJSONArray("images").getJSONObject(0).getString("url"),
+                                                                        expandedObject.getString("energy"),
+                                                                        expandedObject.getString("danceability"),
+                                                                        expandedObject.getString("liveness"),
+                                                                        expandedObject.getString("acousticness"),
+                                                                        trackObject.getString("popularity"),
+                                                                        expandedObject.getString("valence"),
+                                                                        expandedObject.getString("tempo"),
+                                                                        expandedObject.getString("speechiness"),
+                                                                        expandedObject.getString("loudness"),
+                                                                        expandedObject.getString("instrumentalness"),
+                                                                        trackObject.getString("duration_ms")
+                                                                ));
+                                                            }
+
+                                                            Authenticated.trackList = trackArrayList;
+
+                                                            ArrayList<String> displayTracks = new ArrayList<>();
+                                                            for (invTrack track : Authenticated.trackList) {
+                                                                displayTracks.add(String.format("Title: %s\nArtist: %s Album: %s", track.getTitle(), track.getTrackArtist()[0], track.getAlbum()));
+                                                            }
+
+                                                            // Use ListView to display notes
+                                                            ArrayAdapter adapter = new ArrayAdapter(context.getApplicationContext(), android.R.layout.simple_list_item_1, displayTracks);
+                                                            ListView listView = (ListView) activity.findViewById(R.id.resultsList);
+                                                            listView.setAdapter(adapter);
+
+                                                            // Add onItemClickListener
+                                                            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                                                @Override
+                                                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                                                    Log.d("ItemClick", Integer.toString(position));
+                                                                    Log.d("ItemClick", Integer.toString(((int)id)));
+                                                                    invTrack trackExtra = Authenticated.trackList.get(position);
+                                                                    Intent intent = new Intent(context.getApplicationContext(), TrackInfo.class);
+                                                                    intent.putExtra("trackPosition", position);
+                                                                    context.startActivity(intent);
+                                                                }
+                                                            });
+
+                                                            activity.findViewById(R.id.resultsList).setVisibility(View.VISIBLE);
+                                                            activity.findViewById(R.id.tracksListView).setVisibility(View.INVISIBLE);
+                                                            activity.findViewById(R.id.searchBar).setVisibility(View.INVISIBLE);
+                                                            activity.findViewById(R.id.toggleButton).setVisibility(View.INVISIBLE);
+                                                            activity.findViewById(R.id.basicSearch).setVisibility(View.INVISIBLE);
+                                                            activity.findViewById(R.id.findResults).setVisibility(View.INVISIBLE);
+                                                            activity.findViewById(R.id.findGenreResults).setVisibility(View.INVISIBLE);
+                                                            activity.findViewById(R.id.findAdvGenreResults).setVisibility(View.INVISIBLE);
+                                                            activity.findViewById(R.id.genreIncSearch).setVisibility(View.INVISIBLE);
+                                                            activity.findViewById(R.id.genreButton1).setVisibility(View.INVISIBLE);
+                                                            activity.findViewById(R.id.genreButton2).setVisibility(View.INVISIBLE);
+                                                            activity.findViewById(R.id.genreButton3).setVisibility(View.INVISIBLE);
+                                                            activity.findViewById(R.id.genreButton4).setVisibility(View.INVISIBLE);
+                                                            activity.findViewById(R.id.genreButton5).setVisibility(View.INVISIBLE);
+
+                                                        } catch (JSONException e) {
+                                                            e.printStackTrace();
+                                                        }
+                                                    }
+                                                };
+
+                                                RequestQueue queue = Volley.newRequestQueue(context.getApplicationContext());
+                                                StringRequest expandedRequest = RemoteAPI.getTracksAudioFeatures(expanded_listener, sharedPreferences.getString("token", RemoteAPI.TOKEN), trackIDs);
+                                                queue.add(expandedRequest);
+
+
+
+                                            } catch (JSONException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    };
+
+                                    RequestQueue queue = Volley.newRequestQueue(context.getApplicationContext());
+                                    StringRequest trackRequest = RemoteAPI.getTracks(track_listener, sharedPreferences.getString("token", RemoteAPI.TOKEN), trackIDs, Authenticated.MARKET);
+                                    StringRequest tracks_response = RemoteAPI.getTracks(track_listener, sharedPreferences.getString("token", Authenticated.AUTH_TOKEN), trackIDs, Authenticated.MARKET);
+                                    queue.add(trackRequest);
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        };
+
+                        String artist = "";
+                        String track = "";
+                        if (tag < 5) {
+                            track = id;
+                        } else {
+                            artist = id;
+                        }
+
+                        StringRequest searchRequest = RemoteAPI.getRecommendations(search_listener, sharedPreferences.getString("token", RemoteAPI.TOKEN), "", artist, track,
+                                Authenticated.MARKET, 20, acousticnessBarFloat, danceabilityBarFloat, -1, energyBarFloat, instrumentalnessBarFloat,
+                                livenessBarFloat, -1, popularityBarInt, speechinessBarFloat, tempoBarInt, valenceBarFloat);
+                        queue.add(searchRequest);
                     }
                 });
             }
